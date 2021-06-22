9f8c3141e10002083f1abd300b3bb81817d39716
willy0404 <willybernardus@gmail.com>
2021-06-22 20:50:55 +0700

    PJ-12 Update addAddress

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,7 +1,7 @@
 const { mysqldb } = require('../connection');
 const { promisify } = require('util');
 const dba = promisify(mysqldb.query).bind(mysqldb);
-
+const geolib = require('geolib');
 
 module.exports = {
     getAllUsers: (req, res) => {
@@ -19,11 +19,11 @@ module.exports = {
             return res.status(200).send(result);
         });
     },
-    addDataUser: (req, res) => {
+    addPersonalData: (req, res) => {
         try {
             const { id } = req.params;
-            const { first_name, last_name, gender, age, address, city, phone_number, zip } = req.body;
-            let data1 = {
+            const { first_name, last_name, gender, age, phone_number } = req.body;
+            let dataToAdd = {
                 first_name: first_name,
                 last_name: last_name,
                 gender: gender,
@@ -31,18 +31,43 @@ module.exports = {
                 phone_number: phone_number
             };
             let sql = `update users set ? where id = ?`;
-            mysqldb.query(sql, [data1, id], (error) => {
+            mysqldb.query(sql, [dataToAdd, id], (error) => {
                 if (error) return res.status(500).send(error);
-                let data2 = {
-                    address: address,
-                    city: city,
-                    zip: zip
-                };
-                sql = `insert into address set ?`;
+                sql = `select id, first_name, last_name, email, phone_number, gender, age from users where id = ? `;
+                mysqldb.query(sql, [id], (error, result) => {
+                    if (error) return res.status(500).send(error);
+                    return res.status(200).send(result);
+                });
             });
         } catch (error) {
             console.error(error);
             return res.status(500).send({ error: true, message: error.message });
         }
     },
+    addAddress: (req, res) => {
+        try {
+            const { id } = req.params;
+            const { address, city, zip, description, } = req.body;
+            let insertData = {
+                address: address,
+                city: city,
+                zip: zip,
+                description: description,
+                users_id: id
+            };
+            let sql = `insert into address set ?`;
+            mysqldb.query(sql, [insertData], (error) => {
+                if (error) return res.status(500).send({ message: "bad request" });
+                sql = `select a.address, a.city, a.zip, a.description from address a 
+                join users u on a.users_id=u.id where u.id = ?`;
+                mysqldb.query(sql, [id], (error, result) => {
+                    if (error) return res.status(500).send(error);
+                    return res.status(200).send(result);
+                });
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({ error: true, message: error.message });
+        }
+    }
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { authController } = require('../controller');
-const { getAllUsers, getUser, addDataUser } = authController;
+const { getAllUsers, getUser, addPersonalData, addAddress } = authController;
 
 router.get('/all', getAllUsers);
 router.get('/:id', getUser);
 
-router.post('/addData/:id', addDataUser);
+router.post('/addData/:id', addPersonalData);
+router.post('/addAddress/:id', addAddress);
 
 module.exports = router;
