661d827a44ca8c22405f5dc4273c4bb665282085
willy0404 <willybernardus@gmail.com>
2021-06-24 21:27:20 +0700

    PJ-13 Update AddAddress

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,11 @@ const {
   Login,
   Registration,
   All,
-  getAllUsers, 
-  getUser, 
-  addPersonalData, 
-  addAddress 
+  getAllUsers,
+  getUser,
+  addPersonalData,
+  addAddress,
+  getAddress
 } = authController;
 
 router.post("/login", Login);
@@ -17,7 +18,8 @@ router.post("/registration", Registration);
 router.get("/all", All);
 router.get('/all', getAllUsers);
 router.get('/:id', getUser);
+router.get('/address/:users_id', getAddress);
 router.post('/addData/:id', addPersonalData);
-router.post('/addAddress/:id', addAddress);
+router.post('/addAddress/:users_id', addAddress);
 
 module.exports = router;
