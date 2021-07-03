fd307b30e4d095000a093ffedb48d3327b04acf7
dino9611 <aldinorahman36@gmail.com>
2021-07-03 11:18:55 +0700

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -58,7 +58,75 @@ module.exports = {
       return res.status(500).send({ message: "server error" });
     }
   },
-
+  DetailTransaction: async (req, res) => {
+    try {
+      const { uid } = req.user;
+      const { orders_id } = req.query;
+      let sql = `select
+                w.id as warehouse_id 
+                from users u
+                join role r on r.id = u.role
+                join warehouse w on w.role_id = u.role
+                where u.uid = ?`;
+      const dataAdmin = await dba(sql, [uid]);
+      if (dataAdmin[0]) {
+        sql = `select concat(year(o.updated_at),"-",month(o.updated_at),"-",day(o.updated_at)) as dateTime, 
+              concat(hour(o.updated_at),".",minute(o.updated_at)) as hourTime,  
+              concat(u.first_name,' ',u.last_name) as name, 
+              o.status, p.name as productName, 
+              c.category_name as category, 
+              o.invoice_number, 
+              od.qty as quantity, 
+              od.price,
+              w.location as warehouse,
+              od.qty*od.price as amount
+              from orders o
+              join orders_detail od on o.id = od.orders_id
+              join products p on p.id = od.product_id
+              join category c on p.category_id = c.id
+              join warehouse w on o.warehouse_id = w.id
+              join users u on u.id = o.users_id
+              where o.warehouse_id = ? and od.orders_id = ${orders_id}`;
+        const dataTransaction = await dba(sql, dataAdmin[0].warehouse_id);
+        return res.status(200).send(dataTransaction);
+      } else {
+        sql = `select u.id, concat(u.first_name,' ',u.last_name) as name, 
+              u.role 
+              from users u
+              join role r on r.id = u.role
+              where u.uid = ?`;
+        const dataAdminSuper = await dba(sql, [uid]);
+        if (dataAdminSuper[0]) {
+          sql = `select concat(year(o.updated_at),"-",month(o.updated_at),"-",day(o.updated_at)) as dateTime, 
+                concat(hour(o.updated_at),".",minute(o.updated_at)) as hourTime,  
+                concat(u.first_name,' ',u.last_name) as name, 
+                o.status, p.name as productName, 
+                c.category_name as category, 
+                o.invoice_number, 
+                od.qty as quantity, 
+                od.price,
+                w.location as warehouse,
+                o.warehouse_id,
+                od.qty*od.price as amount
+                from orders o
+                join orders_detail od on o.id = od.orders_id
+                join products p on p.id = od.product_id
+                join category c on p.category_id = c.id
+                join warehouse w on o.warehouse_id = w.id
+                join users u on u.id = o.users_id
+                where od.orders_id = ${orders_id}`;
+          // console.log(orders_id);
+          const dataTransaction = await dba(sql);
+          return res.status(200).send(dataTransaction);
+        } else {
+          return res.status(500).send({ message: "data not found" });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  },
   TransactionAdmin: async (req, res) => {
     try {
       const { uid } = req.user;
