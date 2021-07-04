8be02dcc9c02ad22890e2b01a61e447d8119bdde
Algi <algifari843@gmail.com>
2021-07-04 08:11:20 +0700

    PJ-26 switch to PJ-20

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -20,25 +20,32 @@ module.exports = {
 
   getProductPaginate: async (req, res) => {
     try {
-      const { pages, limit } = req.query;
+      const { pages, limit, status } = req.query;
+      let statusSql = "";
       if (!pages || !limit)
         return res.status(400).send({ message: "Pages/limit harus diisi" });
+      if (status) {
+        statusSql = `and c.category_name = ${mysqldb.escape(status)}`;
+      }
       let sql = `select p.id, p.name, p.price, p.description, p.image, sum(pl.qty) as quantity, c.category_name as category, w.location  
                         from products p 
                         left join products_location pl on p.id = pl.products_id 
                         left join warehouse w on pl.warehouse_id = w.id 
                         join category c on p.category_id = c.id 
-                        where p.is_deleted = 0 group by p.id 
+                        where p.is_deleted = 0 ${statusSql}
+                        group by p.id 
                         limit ${mysqldb.escape(
                           (parseInt(pages) - 1) * 12
                         )},${mysqldb.escape(parseInt(limit))}`;
       const dataProduct = await dba(sql);
-      sql = `select count(*) as totaldata from products where is_deleted = 0`;
+      sql = `select count(*) as totaldata from products p
+              join category c on p.category_id = c.id
+              where p.is_deleted = 0 ${statusSql};`;
       const countProduct = await dba(sql);
-      console.log(countProduct[0].totaldata);
-      return res
-        .status(200)
-        .send({ dataProduct, totaldata: countProduct[0].totaldata });
+      return res.status(200).send({
+        dataProduct,
+        totaldata: countProduct[0].totaldata,
+      });
     } catch (error) {
       return res.status(500).send({ message: "server error" });
     }
@@ -55,6 +62,16 @@ module.exports = {
                   `;
       const productDetail = await dba(sql, [id]);
       return res.status(200).send(productDetail);
+    } catch (error) {
+      return res.status(500).send({ message: "server error" });
+    }
+  },
+
+  getCategory: async (req, res) => {
+    try {
+      let sql = `select category_name from category;`;
+      const dataCategory = await dba(sql);
+      return res.status(200).send(dataCategory);
     } catch (error) {
       return res.status(500).send({ message: "server error" });
     }
--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { ProductController } = require("../controllers");
 
-const { getAllProducts, getProductPaginate, getProductDetail } =
+const { getAllProducts, getProductPaginate, getProductDetail, getCategory } =
   ProductController;
 
 router.get("/all", getAllProducts);
 router.get("/paging", getProductPaginate);
+router.get("/category", getCategory);
 router.get("/productDetail/:id", getProductDetail);
 
 module.exports = router;
