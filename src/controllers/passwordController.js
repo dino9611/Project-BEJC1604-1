21e2be36ca9bc0d79c14533f5b7889d8dfef647b
willy0404 <willybernardus@gmail.com>
2021-07-05 22:02:53 +0700

    PJ-5 forgot password

--- src/controllers/passwordController.js
+++ src/controllers/passwordController.js
@@ -3,22 +3,27 @@ const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
-const hassingPass = require('../helpers/hassingPass');
+const hashingPass = require('../helpers/hassingPass');
 const dba = promisify(mysqldb.query).bind(mysqldb);
+const { createForgotPassToken } = require('../helpers/createToken');
+const transporter = require('../helpers/transporter');
+
+const link = 'http://localhost:3000/resetpassword/';
 
 module.exports = {
-    changePassword: async (req, res) => {
+    resetPassword: async (req, res) => {
         try {
             const { newPassword } = req.body;
-            const { id } = req.params;
+            // console.log(newPassword);
+            const { uid } = req.user;
             let dataChange = {
-                password: newPassword
+                password: hashingPass(newPassword)
             };
-            let sql = `update users set ? where id = ?`;
-            await dba(sql, [dataChange, id]);
+            let sql = `update users set ? where uid = ?`;
+            await dba(sql, [dataChange, uid]);
             console.log('Password change');
-            sql = `select * from users where id = ? `;
-            let hasil = await dba(sql, [id]);
+            sql = `select * from users where uid = ? `;
+            let hasil = await dba(sql, [uid]);
             console.log(hasil);
             return res.status(200).send(hasil[0]);
         } catch (error) {
@@ -27,6 +32,33 @@ module.exports = {
         }
     },
     forgotPassword: async (req, res) => {
+        try {
+            const { email } = req.body;
+            let sql = `select uid from users where email = ?`;
+            let user = await dba(sql, email);
+            if (user.length) {
+                let filePath = path.resolve(__dirname, '../template/emailForgotPassword.html');
+                const htmlRender = fs.readFileSync(filePath, 'utf-8');
+                const template = handlebars.compile(htmlRender);
+                let dataToken = {
+                    uid: user[0].uid
+                };
+                const tokenForgot = createForgotPassToken(dataToken);
+                const htmlToEmail = template({ link: link + tokenForgot });
+                await transporter.sendMail({
+                    from: "Admin Fournir <omiputrakarunia@gmail.com>",
+                    to: email,
+                    subject: `Please change your password`,
+                    html: htmlToEmail,
+                });
+                return res.status(200).send({ message: 'A reset link has sent to your email!' });
+            } else {
+                return res.status(400).send({ message: 'Email unregistered' });
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({ message: 'Server error' });
+        }
 
     }
 };
--- src/routes/passwordRoutes.js
+++ src/routes/passwordRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { passwordController } = require('../controllers');
 
-const { changePassword, forgotPassword } = passwordController;
+const { resetPassword, forgotPassword } = passwordController;
+const { verifyEmailForgotToken } = require('../helpers/verifyToken');
 
-router.patch('/change/:id', changePassword);
-router.put('/forgot', forgotPassword);
+router.post('/forgot', forgotPassword);
+router.put('/resetpassword', verifyEmailForgotToken, resetPassword);
 
 module.exports = router;
