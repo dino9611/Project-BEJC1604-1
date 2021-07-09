81e6a2dea422ea55471ae759ad5d43de4b9ab4ac
Algi <algifari843@gmail.com>
2021-07-09 13:45:07 +0700

    update adminController (getProductAdmin)

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -530,20 +530,31 @@ module.exports = {
 
   getProductAdmin: async (req, res) => {
     try {
-      const { pages, limit } = req.query;
+      const { pages, limit, status, search } = req.query;
+      let statusSql = "";
+      let searchSql = "";
       if (!pages || !limit)
         return res.status(400).send({ message: "Pages/limit harus diisi" });
-      let sql = `select p.id, p.name, p.price, p.image, p.category_id, sum(pl.qty) as quantity, c.category_name as category, w.location  
+      if (status) {
+        statusSql = `and c.category_name = ${mysqldb.escape(status)}`;
+      }
+      if (search) {
+        searchSql = `and p.name like ${mysqldb.escape("%" + search + "%")}`;
+      }
+      let sql = `select p.id, p.name, p.price, p.image, p.category_id, ifnull(sum(pl.qty), 0) as quantity, c.category_name as category, w.location  
                         from products p 
                         left join products_location pl on p.id = pl.products_id 
                         left join warehouse w on pl.warehouse_id = w.id 
                         join category c on p.category_id = c.id 
-                        where p.is_deleted = 0 group by p.id 
+                        where p.is_deleted = 0 ${statusSql} ${searchSql} 
+                        group by p.id 
                         limit ${mysqldb.escape(
-                          (parseInt(pages) - 1) * 10
+                          parseInt(pages) * parseInt(limit)
                         )},${mysqldb.escape(parseInt(limit))}`;
       const dataProduct = await dba(sql);
-      sql = `select count(*) as totaldata from products where is_deleted = 0`;
+      sql = `select count(*) as totaldata from products p
+              join category c on p.category_id = c.id
+              where p.is_deleted = 0 ${statusSql} ${searchSql}`;
       const countProduct = await dba(sql);
       return res
         .status(200)
